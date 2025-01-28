da55c5527b49d8a2aa95a97b79a35c75edde0b54
parsasabetz <parsa2003sabet@icloud.com>
2025-01-28 21:22:13 +0330

    feat(metadata): Add get_file_metadata for public export

--- src/audiopro/__init__.py
+++ src/audiopro/__init__.py
@@ -1,10 +1,12 @@
 from .process import analyze_audio
 from .extractor import extract_features
 from .monitor import monitor_cpu_usage, print_performance_stats
+from .metadata import get_file_metadata
 
 __all__ = [
     "analyze_audio",
     "extract_features",
     "monitor_cpu_usage",
-    "print_performance_stats"
+    "print_performance_stats",
+    "get_file_metadata",
 ]
