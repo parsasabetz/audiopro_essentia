ad640080c2318430dc1484a9b2f9303c5f0a5562
parsasabetz <parsa2003sabet@icloud.com>
2025-02-05 11:36:10 +0330

    feat(exceptions): add custom exceptions for audio processing errors

--- src/audiopro/audio/__init__.py
+++ src/audiopro/audio/__init__.py
@@ -3,17 +3,16 @@ Audio processing module for feature extraction and analysis.
 """
 
 from .models import FrameFeatures
-from .processors import (
-    get_frequency_bins,
-    compute_frequency_bands,
-    process_frame
-)
+from .processors import get_frequency_bins, compute_frequency_bands, process_frame
 from .extractor import extract_features
+from .exceptions import FeatureExtractionError, ExtractionPipelineError
 
 __all__ = [
-    'FrameFeatures',
-    'get_frequency_bins',
-    'compute_frequency_bands',
-    'process_frame',
-    'extract_features'
+    "FrameFeatures",
+    "get_frequency_bins",
+    "compute_frequency_bands",
+    "process_frame",
+    "extract_features",
+    "FeatureExtractionError",
+    "ExtractionPipelineError",
 ]
