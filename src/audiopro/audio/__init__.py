dc3692ffadd6d2098c6ab5ab28d2aaf78e99755c
parsasabetz <parsa2003sabet@icloud.com>
2025-02-15 17:22:29 +0330

    refactor: update audio module exports to include spectrogram processing and remove unused frequency functions

--- src/audiopro/audio/__init__.py
+++ src/audiopro/audio/__init__.py
@@ -1,21 +1,22 @@
 """Audio processing module."""
 
 from .models import FrameFeatures
-from .processors import get_frequency_bins, compute_frequency_bands, process_frame
+from .processors import process_frame
 from .extractor import extract_features
 from .audio_loader import load_and_preprocess_audio
 from .validator import validate_audio_file, validate_audio_signal
 from .frame_generator import create_frame_generator
+from .spectrogram import compute_spectrogram, process_spectral_features
 
 
 __all__ = [
     "FrameFeatures",
-    "get_frequency_bins",
-    "compute_frequency_bands",
     "process_frame",
     "extract_features",
     "load_and_preprocess_audio",
     "validate_audio_file",
     "validate_audio_signal",
     "create_frame_generator",
+    "compute_spectrogram",
+    "process_spectral_features",
 ]
